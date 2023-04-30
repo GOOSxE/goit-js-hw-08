eac07b279f0420cd1882d1fe8e9708e9b784a68d
GOOSxE <119361095+GOOSxE@users.noreply.github.com>
2023-04-30 13:20:09 +0000

    Deploying to gh-pages from @ GOOSxE/goit-js-hw-08@25ee7f4314dac6559178cad9ec8420554e69f309 🚀

--- 02-video.5dd2882a.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe");new Vimeo.Player(e).on("play",(function(){console.log("played the video!")}));
-//# sourceMappingURL=02-video.5dd2882a.js.map
--- 02-video.df417164.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe");new Vimeo.Player(e).on("play",(function(){console.log("played the video!")}))}();
-//# sourceMappingURL=02-video.df417164.js.map
