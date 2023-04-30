25ee7f4314dac6559178cad9ec8420554e69f309
GOOSxE <der.goose.4308@gmail.com>
2023-04-30 15:19:42 +0200

    fff

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,14 @@
 // *
 
+import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-const playerUrl = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 // *
 
-playerUrl.on('play', function() {
-    console.log('played the video!');
+player.on('play', function () {
+  console.log('played the video!');
 });
 
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
