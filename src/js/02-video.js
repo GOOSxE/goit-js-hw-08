6e3b6f3184d8071cb1b02745f2432a2491ba6f3a
GOOSxE <der.goose.4308@gmail.com>
2023-04-30 14:36:04 +0200

    installed vimeoPlayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+// *
+
+const iframe = document.querySelector('iframe');
+const playerUrl = new Vimeo.Player(iframe);
+// *
+
+playerUrl.on('play', function() {
+    console.log('played the video!');
+});
 
