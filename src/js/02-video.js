e32438d81c89631e94ad96eca2f52228e6e169ec
GOOSxE <der.goose.4308@gmail.com>
2023-05-01 16:14:58 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 // *
 import Player from '@vimeo/player';
-import { isNumber, throttle } from 'lodash';
+import throttle from 'lodash';
 // *
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -10,13 +10,13 @@ const STORAGE_KEY = 'videoplayer-current-time';
 // ? // Створюю ключ сховища
 
 player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
-// ? // Після перевірки встановив поточний час із сховища або 0 при false 
+// ? // Після перевірки встановив поточний час із сховища або 0 при false
 
 const onTimeUpdate = function () {
   player.getCurrentTime().then(function (seconds) {
     localStorage.setItem(STORAGE_KEY, seconds);
   });
 };
-player.on('timeupdate', throttle(onTimeUpdate, (wait = 1000)));
+player.on('timeupdate', throttle(onTimeUpdate, [(wait = 1000)]));
 // ? // При зміні поточного тайму перегляду записав нові значення в сховище
-// ? Та 
+// ? Та
