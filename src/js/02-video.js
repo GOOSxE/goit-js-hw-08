6fd5690db49061471894150d963277e2d8f53ea3
GOOSxE <der.goose.4308@gmail.com>
2023-05-03 19:23:36 +0200

    completed hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 // *
 import Player from '@vimeo/player';
-import throttle  from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 // *
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -12,10 +12,7 @@ const STORAGE_KEY = 'videoplayer-current-time';
 player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
 // ? // Після перевірки встановив поточний час із сховища або 0 при false
 
-const onTimeUpdate = function () {
-  player.getCurrentTime().then(function (seconds) {
-    localStorage.setItem(STORAGE_KEY, seconds);
-  });
-};
+const onTimeUpdate = time => localStorage.setItem(STORAGE_KEY, time.seconds);
+
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
 // ? // При зміні поточного тайму перегляду записав нові значення в сховище
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,56 @@
-// * 
+// *
 import throttle from 'lodash.throttle';
 // *
 const inputFormUrl = document.querySelector('.feedback-form');
 const emailInputUrl = document.querySelector('input');
 const messageInputUrl = document.querySelector('textarea');
-// ? // Додав селектори
-
 const FORM_STORAGE_KEY = 'feedback-form-state';
-const settingsArray = {
-  email: 'example@gmail.com',
-  message: 'message example...',
-};
-// ? // Створив ключ сховища та масив з параметрами
+// ? // Додав селектори та створив ключ сховища ;
 
 if (localStorage.getItem(FORM_STORAGE_KEY)) {
   const { email, message } = JSON.parse(localStorage.getItem(FORM_STORAGE_KEY));
   emailInputUrl.value = email;
   messageInputUrl.value = message;
 }
+
+const settingsArray = {
+  email: emailInputUrl.value || "",
+  message: messageInputUrl.value || "",
+};
 // ? // Після перевірки на наявність даних в сховищі - додаються значення в інпути
+// ? Та створюється об'єкт з параметрами ;
 
 inputFormUrl.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(event) {
   if (event.target.name === 'email' || event.target.name === 'message') {
-    settingsArray.email = emailInputUrl.value;
-    settingsArray.message = messageInputUrl.value;
+    settingsArray.email = emailInputUrl.value.trim();
+    settingsArray.message = messageInputUrl.value.trim();
     localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(settingsArray));
   }
 }
 // ? // При інпутах працює функція що додає в масив параметрів поточні значенняя інпутів
-// ? Після чого додає масив поточних значень методом JSON.stringify в локальне сховище 
+// ? Після чого додає масив поточних значень методом JSON.stringify в локальне сховище ;
 
 inputFormUrl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(localStorage.getItem(FORM_STORAGE_KEY));
-  localStorage.clear();
-  inputFormUrl.reset()
+  if (
+    emailInputUrl.value.trim() === '' ||
+    messageInputUrl.value.trim() === ''
+  ) {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+  console.log(settingsArray);
+
+  localStorage.removeItem(FORM_STORAGE_KEY);
+  inputFormUrl.reset();
+  
+  settingsArray.email = '';
+  settingsArray.message = '';
 }
-// ? // При сабміті форми функція виводить поточні значення інпутів в консоль
-// ? Після чого очищується сховище та інпути 
+// ? // При сабміті форми функція виводить поточні значення об'єкта параметрів в консоль
+// ? Після чого очищується ТІЛЬКИ ОДИН ключ сховища, інпути,
+// ? Та об'єкт з параметрами даних ;
